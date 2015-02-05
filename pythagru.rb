052b2d148436e5e5def0d545de24c0cc733f403d
Regis <rpiduino@gmail.com>
2015-02-05 09:28:54 -0500

    Create pythagru.rb
    This is the Pythagorean Theorem in Ruby!
    
    Solves for c!
    
    There are pauses to make the program seem more traditional. Otherwise I can remove them if they are annoying.

---
+++ pythagru.rb
@@ -0,0 +1,39 @@
+#This will be a way to solve a right angle triangle's third (longest [c]) side.
+#We will use Pathagorean's Theorem to solve it using ruby
+#This will be designed to simply ask the user what a and b are
+
+
+print "Do you know the value for a and b? (Yes/No)"
+  i1 = gets.chomp
+
+if i1 == "Yes"
+  puts "Great! I will ask you what a and b are next."
+else
+  puts "Once you find out the values for a and b, I can help you!"
+end
+
+sleep(2)
+
+puts "What is a?"
+  a = gets.chomp.to_i
+
+sleep(1)
+
+puts "What is b?"
+  b = gets.chomp.to_i
+
+sleep(1)
+
+c2 = (a*a) + (b*b)
+
+sleep(1)
+
+require 'complex'
+include Math
+
+Complex(c2)
+sqrt(c2)
+
+puts "Your answer below:"
+puts sqrt(c2)
+puts "There you go!"
