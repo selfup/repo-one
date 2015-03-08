b7271ac2167b5b852e28a14db77a46bcc924982d
Regis <boudinot.regis@yahoo.com>
2015-03-08 20:28:16 +0000

    c9 up

--- LRTHW/jex30.rb
+++ LRTHW/jex30.rb
@@ -1,16 +1,26 @@
 print "How many people are there? "
 i1 = gets.chomp.to_i
-print "How many cats do you see? "
+print "How many cars do you see? "
 i2 = gets.chomp.to_i
 print "How many trucks are you avoiding right now? "
 i3 = gets.chomp.to_i
 
 people = i1; cars = i2; trucks = i3
 
-if cars > people
+if cars > people || cars > trucks 
     puts "We should take the cars"
     elsif cars < people
     puts "We should not take the cars"
 else
     puts "We can't decide."
 end
+
+if trucks > cars
+    puts "That's too many trucks."
+    elsif trucks < cars
+    puts "Maybe we would take the trucks."
+else
+    puts "Fine, let's stay home then."
+end
+
+
