55cd83f3dbd05ad97810786fa91232d9662f411a
Regis <boudinot.regis@yahoo.com>
2015-03-08 20:37:33 +0000

    c9 update: one line if's and boolean stuff

--- LRTHW/jex30.rb
+++ LRTHW/jex30.rb
@@ -4,7 +4,11 @@ print "How many cars do you see? "
 i2 = gets.chomp.to_i
 print "How many trucks are you avoiding right now? "
 i3 = gets.chomp.to_i
+print "Are you feeling courageous?"
+i4 = gets.chomp.downcase
 
+if i4 == "yes"; i4 = 10000; else; i4 = 0; end
+    
 people = i1; cars = i2; trucks = i3
 
 if cars > people || cars > trucks 
@@ -15,7 +19,7 @@ else
     puts "We can't decide."
 end
 
-if trucks > cars
+if trucks > cars || i4 == 0
     puts "That's too many trucks."
     elsif trucks < cars
     puts "Maybe we would take the trucks."
@@ -23,4 +27,7 @@ else
     puts "Fine, let's stay home then."
 end
 
+if i4 == 10000
+    puts "Take it all"
+end
 
