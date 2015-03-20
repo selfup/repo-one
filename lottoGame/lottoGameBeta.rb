338b468816a003f74d2a92dfb22ed416560cc1d8
Regis <boudinot.regis@yahoo.com>
2015-03-20 01:04:45 +0000

    Updated lottoGameBeta! via c9.io :)

--- lottoGame/lottoGameBeta.rb
+++ lottoGame/lottoGameBeta.rb
@@ -19,12 +19,13 @@ puts "#{a1}.."
 # choosing a random number to define a new variable
 # this number will be used to choose what pushed number to pull from the a1 array
 rn = prng.rand(i1..i2); rm = (rn - 1) # rm has to equal (rn -1) or else the variable will be too far over the array
-sleep(2); puts"OK, thanks!"
+sleep(2); puts "OK, thanks!"
 puts "Now give me your best guess as to what number will be drawn... "; i3 = gets.chomp.to_i
+puts "Lucky you! I am going to give you two chances! Give me a second number to win with :) "; i4 = gets.chomp.to_i
 sleep(2); puts "The number is: #{a1[rm]}"; sleep(2)
 puts "Were you right?"; sleep(2)
 
-if i3 == a1[rn]; puts "NICE! YOU WERE RIGHT!"; else puts "Nooooope! Sorry!"; end
+if i3 == a1[rn]; puts "NICE! YOU WERE RIGHT!"; elsif i4 == a1[rn]; puts "Way to get it on your second try!"; else puts "Nooooope! Sorry!"; end
 
-# Git Test... 
+# c9 edit --
 
