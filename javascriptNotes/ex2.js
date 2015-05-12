8e3e28e3bc90c35b182615609a1d5e9bc5e59c72
selfup <boudinot.regis@yahoo.com>
2015-05-12 14:20:18 -0500

    New Folder

---
+++ javascriptNotes/ex2.js
@@ -0,0 +1,40 @@
+var name = 'Mark';
+
+//Now if I change my name to Ace//
+
+//And declare after the Mark var line//
+
+//Javascript will just print out Ace//
+
+//Example Ahead!//
+
+var name = 'Ace';
+
+//I have now changed the variable (within the Global Scope)//
+
+//Javascript will forever think var "name" = 'Ace' until told otherwise//
+
+/*Once a varirable has been declared, you don't have to declare it again.
+You just assign a new vlaue to it.*/
+
+var nationality;
+nationality = 'U.S.';
+
+//Remember to give your variables meaningful names//
+
+//This way you can understand your code in the future//
+
+var floogle = 'Mark';
+
+var name = 'Mark';
+
+var floogle = 'Mark';
+
+var lessonAuthor = 'Mark';
+var guyWhoKeepsSayingHisOwnName = 'Mark';
+var x = 'Mark';
+
+var lastName = 'Smith';
+var cityOfOrigin = 'New Orleans';
+var aussieGreeting = "g'Day";
+
