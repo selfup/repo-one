4f81d078ea701c423d9df69b14db5365f513a48e
selfup <boudinot.regis@yahoo.com>
2015-05-13 15:17:04 -0500

    Renaming files

---
+++ javascriptNotes/ex02.js
@@ -0,0 +1,40 @@
+var name = 'John';
+
+//Now if I change my name to Will//
+
+//And declare after the Mark var line//
+
+//Javascript will just print out Will//
+
+//Example Ahead!!!//
+
+var name = 'Will';
+
+//I have now changed the variable (within the Global Scope)//
+
+//Javascript will forever think var "name" = 'Will' until told otherwise//
+
+/*Once a varirable has been declared, you don't have to declare it again.
+You just assign a new vlaue to it.*/
+
+var nationality;
+nationality = 'U.S.';
+
+//Remember to give your variables meaningful names//
+
+//This way you can understand your code in the future//
+
+var floogle = 'John';
+
+var name = 'John';
+
+var floogle = 'John';
+
+var lessonAuthor = 'John';
+var guyWhoKeepsSayingHisOwnName = 'John';
+var x = 'John';
+
+var lastName = 'Smith';
+var cityOfOrigin = 'Boston';
+var aussieGreeting = "g'Day";
+
---
+++ javascriptNotes/ex04.js
@@ -0,0 +1,5 @@
+/*This just covers what is LEGAL or ILLEGAL as variable nomenclature.
+
+Screenshots or Reference Cards.
+
+Moving on!*/
