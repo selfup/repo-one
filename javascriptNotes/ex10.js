d9fa6c15cc5d7b91e33eec71e67f9c76427d5d5f
selfup <boudinot.regis@yahoo.com>
2015-05-13 15:10:31 -0500

    Some simple IF, but a wild lesson

---
+++ javascriptNotes/ex10.js
@@ -0,0 +1,42 @@
+//IF statements and prompt..ohhhhh yeaaa//
+
+var x = prompt('What does a does wag?');
+
+if (x === 'tail') {
+	alert('Correct!');
+} 
+else {
+	alert("Its tail! Not..that!");
+} 
+
+//I am testing out the different ways to format stuff.//
+
+
+/*
+
+BOTH === and !== are strict comparison operators.
+
+************************************************************
+
+NaN !== Nan for example. Tough cookie
+
+************************************************************
+
+Null == Undefined 
+
+HOWEVER
+
+Null === Undefined - IS FALSE
+
+************************************************************
+
+Boolean operands are stricly equal if both are true or false
+
+Hopefully this clears some things up!
+
+*/
+
+
+
+
+
