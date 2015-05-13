e238a2dd2cfb1b8d0cf897fb0709476d057eaa52
selfup <boudinot.regis@yahoo.com>
2015-05-13 18:26:16 -0500

    New files, nothing crazy

---
+++ javascriptNotes/ex16.js
@@ -0,0 +1,19 @@
+//Adding and Removing stuff//
+
+//Same as ruby with 'pop' and 'push'//
+
+var idx1 = [];
+
+idx1.push('ex1', 'ex2');
+console.log(idx1);
+
+//Now if I just want to remove ex2 I can pop idx1//
+
+idx1.pop();
+console.log(idx1);
+
+
+//Sweet//
+
+//END//
+
