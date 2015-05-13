6daf5cca7dcbaa840ce4a87ab6ce1b7b4599b6d4
selfup <boudinot.regis@yahoo.com>
2015-05-13 14:54:45 -0500

    Number 9 or number 9

---
+++ javascriptNotes/ex9.js
@@ -0,0 +1,32 @@
+//prompts//
+
+var spec = prompt('Your species?', 'human');
+
+//DO NOT RUN THIS IN NODE!//
+//Won't work//
+
+//Cmd + Option + I ..or.. f12 for non OSX//
+//Then copy pastaaaa into console//
+
+//Now to use a neat example//
+
+var hello = 'Hey are you enjoying this so far?';
+var defAns = 'Yea for sure!';
+var spec2 = prompt(hello, defAns);
+
+//Time to try this out!//
+
+//END//
+
+//IMPORTANT//
+
+/*
+
+Just simply declaring a variable as a prompt
+will initiate a prompt.
+
+You do not need to call it at a later date.
+
+Very interesting behavior.
+
+*/
