560d7d7a61687245dd49f21cc23a278a39afc5fc
Regis <boudinot.regis@yahoo.com>
2015-05-16 13:19:20 -0500

    New setup for now

---
+++ javascriptNotes/ex19.js
@@ -0,0 +1,7 @@
+// This will be on my Win8.1 laptop //
+// Using VIM via Git Bash //
+// Macbook has a very loose trackpad.. //
+// Will bring it in for repair soon //
+// Might use Visual Studio Code instead //
+// .vimrc not working right //
+
