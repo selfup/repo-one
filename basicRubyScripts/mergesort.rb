c7a2db26e90585bd51738ec3194f07b1676ca60f
Regis Boudinot <selfup@users.noreply.github.com>
2015-07-13 13:35:46 -0600

    Formatting

--- basicRubyScripts/mergesort.rb
+++ basicRubyScripts/mergesort.rb
@@ -9,6 +9,29 @@ def mid_point(a)
   a.size / 2
 end
 
+################ Figures out if the observed array is empty
+def is_it_empty(i)
+  i.empty?
+end
+
+##################### Until the array is empty...do the following awesomeness (aka << the numbers in order)
+def until_empty(l , r)
+	until is_it_empty(l) or is_it_empty(r)
+		if l.first <= r.first
+			@sorted << l.shift
+		else
+			@sorted << r.shift
+		end
+	end
+end
+
+############## Add all of the things
+def add_in_order(l, r)
+	@sorted = []
+  until_empty(l, r)
+  @sorted.concat(l).concat(r)
+end
+
 ########## Etablishes array size
 ########## Figures out the left and right sides
 ########## Waits until arrays are less than or equal to: an index value of 1
@@ -18,36 +41,13 @@ end
 ########## Then it adds already sorted arrays.
 ########## It does all this from left to right until complete
 def merge_and_sort_and_merge(a)
-  ar = a.size
-  l = a[0, mid_point(a)]
-  r = a[mid_point(a), ar - mid_point(a)]
-  if ar <= 1
-    return a
-  end
+	ar = a.size
+	l = a[0, mid_point(a)]
+	r = a[mid_point(a), ar - mid_point(a)]
+	if ar <= 1
+		return a
+	end
   add_in_order(merge_and_sort_and_merge(l), merge_and_sort_and_merge(r))
-end
-
-################ Figures out if the observed array is empty
-def is_it_empty(i)
-  i.empty?
-end
-
-##################### Until the array is empty...do the following awesomeness (aka << the numbers in order)
-def until_empty(l , r)
-  until is_it_empty(l) or is_it_empty(r)
-    if l.first <= r.first
-	@sorted << l.shift
-    else
-	@sorted << r.shift
-    end
-  end
-end
-
-############## Add all of the things
-def add_in_order(l, r)
-  @sorted = []
-  until_empty(l, r)
-  @sorted.concat(l).concat(r)
 end
 
 print "#{merge_and_sort_and_merge(values)} \n"
